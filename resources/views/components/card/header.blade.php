a7c33ee6e50e7499772466098519f6b08ac1d194
Frederik Van Hecke <frederik@kluster.dev>
2025-01-27 18:25:09 +0100

    Adding basic card components

---
+++ resources/views/components/card/header.blade.php
@@ -0,0 +1,13 @@
+<div class="card-header">
+@if($title != "" || $icon != "")
+    <h3 class="card-title">
+@if(isset($icon) && $icon != "")
+        <x-icon.tabler icon="{{ $icon }}" style="font-size: 14pt; margin-right: 5px;"/>
+@endif
+        {{ $title }}
+    </h3>
+@endif
+@if($slot->isNotEmpty())
+    <div class="card-actions">{{ $slot }}</div>
+@endif
+</div>
