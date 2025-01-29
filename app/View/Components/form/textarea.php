b946450a1f208d3725ac8d4acd12611ee5155bf3
Frederik Van Hecke <frederik@kluster.dev>
2025-01-29 21:12:43 +0100

    Adding textarea input component

---
+++ app/View/Components/form/textarea.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\View\Components\form;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class textarea extends Component
+{
+    //
+    // PARAMETERS
+    //
+
+    // Name for the textarea
+    public $name;
+
+    // Number of rows for the textarea
+    public $rows;
+
+    // Placeholder for the textarea
+    public $placeholder;
+
+    // Value for the text area
+    public $value;
+
+    // Flag indicating whether textarea is disabled
+    public $disabled;
+
+    // Flag indicating whether textarea is read-only
+    public $readonly;
+
+
+    //
+    // CONSTRUCTORS
+    //
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($name = "", $rows = 5, $placeholder = "", $value = "", $disabled = false, $readonly = false)
+    {
+        $this->name = $name;
+        $this->rows = $rows;
+        $this->placeholder = $placeholder;
+        $this->value = $value;
+        $this->disabled = $disabled;
+        $this->readonly = $readonly;
+    }
+
+
+    //
+    // METHODS
+    //
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.form.textarea');
+    }
+}
