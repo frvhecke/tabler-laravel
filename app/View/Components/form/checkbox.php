8fb6174007ec6f8026188cc4d7513dfd00b4fa19
Frederik Van Hecke <frederik@kluster.dev>
2025-02-01 15:00:49 +0100

    Adding checkbox component

---
+++ app/View/Components/form/checkbox.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\View\Components\form;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class checkbox extends Component
+{
+    //
+    // PARAMETERS
+    //
+
+    // Name for the checkbox
+    public $name;
+
+    // Text for the checkbox
+    public $text;
+
+    // Value for the checkbox
+    public $value;
+
+    // Flag indicating whether the checkbox is selected
+    public $selected;
+
+    // Flag indicating whether the checkbox is disabled
+    public $disabled;
+
+    // Flag indicating whether the checkbox is inline displayed
+    public $inline;
+
+
+    //
+    // CONSTRUCTORS
+    //
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($name = "", $text = "", $value = "", $selected = false, $disabled = false, $inline = false)
+    {
+        $this->name = $name;
+        $this->text = $text;
+        $this->value = $value;
+        $this->selected = $selected;
+        $this->disabled = $disabled;
+        $this->inline = $inline;
+    }
+
+
+    //
+    // METHODS
+    //
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.form.checkbox');
+    }
+}
---
+++ resources/views/components/form/checkbox.blade.php
@@ -0,0 +1,4 @@
+<label @class(['form-check', 'form-check-inline' => $inline])>
+  <input class="form-check-input" type="checkbox" @if(!empty($value)) value="{{ $value }}" @endif @if(!empty($name)) name="{{ $name }}" @endif @if($selected) checked @endif @if($disabled) disabled @endif>
+  <span class="form-check-label">{{ $text }}</span>
+</label>
