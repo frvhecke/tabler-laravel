54392a85008686c5ef49f31b91c38db52f19ecae
Frederik Van Hecke <frederik@kluster.dev>
2025-02-04 06:33:34 +0100

    Fixing standard col state and adding center property

--- resources/views/components/grid/col.blade.php
+++ resources/views/components/grid/col.blade.php
@@ -13,7 +13,8 @@
   'colmdauto' => false,
   'collgauto' => false,
   'colxlauto' => false,
+  'center' => false,
 ])
-<div {{ $attributes->class(['col-'.$size => $size>0, 'col-sm-'.$sm => $sm>0, 'col-md-'.$md => $md>0, 'col-lg-'.$lg => $lg>0, 'col-xl-'.$xl => $xl>0, 'col-sm' => $colsm, 'col-md' => $colmd, 'col-lg' => $collg, 'col-xl' => $colxl, 'col-auto' => $colauto, 'col-sm-auto' => $colsmauto, 'col-md-auto' => $colmdauto, 'col-lg-auto' => $collgauto, 'col-xl-auto' => $colxlauto]) }}>
+<div @class(['col' => $size+$sm+$md+$lg+$xl==0 && !$colsm && !$colmd && !$collg && !$colxl && !$colauto && !$colsmauto && !$colmdauto && !$collgauto && !$colxlauto,'col-'.$size => $size>0, 'col-sm-'.$sm => $sm>0, 'col-md-'.$md => $md>0, 'col-lg-'.$lg => $lg>0, 'col-xl-'.$xl => $xl>0, 'col-sm' => $colsm, 'col-md' => $colmd, 'col-lg' => $collg, 'col-xl' => $colxl, 'col-auto' => $colauto, 'col-sm-auto' => $colsmauto, 'col-md-auto' => $colmdauto, 'col-lg-auto' => $collgauto, 'col-xl-auto' => $colxlauto, 'align-self-center' => $center])>
   {{ $slot }}
 </div>
