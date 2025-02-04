daf0f28365f1566311d717e9f09ac4511f6682be
Frederik Van Hecke <frederik@kluster.dev>
2025-02-04 06:27:10 +0100

    Fixing undefined properties for buttons

--- resources/views/components/button/outline.blade.php
+++ resources/views/components/button/outline.blade.php
@@ -2,11 +2,11 @@
   'type' => 'button',     // Type of the button ('button' or 'link')
   'href' => '#',          // Destination for the link
   'target' => '_self',    // Target for the link
-  'color',                // Color for the button
+  'color' => '',          // Color for the button
   'fullwidth' => false,   // Full width state of the button
   'active' => false,      // Active state of the button
   'disabled' => false,    // Disabled state of the button
-  'arialabel'             // Aria label for the button
+  'arialabel' => ''       // Aria label for the button
 ])
 @if($type =='link')
 <a href="{{ $href }}" target="{{ $target }}" @class(['btn', 'btn-outline-'.$color => !empty($color), 'w-100' => $fullwidth, 'active' => $active, 'disabled' => $disabled]) @if(!empty($arialable)) {{ $attributes->merge(['aria-label' => $arialabel]) }} @endif>
