8d71736c56ffa30470dde389d65f65b7638ae688
Frederik Van Hecke <frederik@kluster.dev>
2025-02-09 11:02:27 +0100

    Fix icon class propagation

--- resources/views/components/icon/tabler.blade.php
+++ resources/views/components/icon/tabler.blade.php
@@ -2,4 +2,4 @@
   'icon',         // Name of the tabler icon
   'color' => ''   // Color for the icon
 ])
-<i @class(['icon ti', 'ti-'.$icon => !empty($icon), 'text-'.$color => !empty($color)])></i>
+<i @class(['icon ti', 'ti-'.$icon => !empty($icon), 'text-'.$color => !empty($color), $attributes["class"]])></i>
