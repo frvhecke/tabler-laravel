128db31e1c33998ec52daf34b02a9c54a5e8a046
Frederik Van Hecke <frederik@kluster.dev>
2025-03-05 12:55:02 +0100

    Adding size options to badges

--- resources/views/components/badge/light.blade.php
+++ resources/views/components/badge/light.blade.php
@@ -1,7 +1,9 @@
 @props([
   'color' => 'default',
   'scrollable' => false,
+  'sm' => false,
+  'lg' => false
 ])
-<span {{ $attributes->class([ 'badge' => true, 'bg-'.$color.'-lt' => $color ]) }}>
+<span {{ $attributes->class([ 'badge' => true, 'bg-'.$color.'-lt' => $color, 'badge-sm' => $sm, 'badge-lg' => $lg ]) }}>
   {{ $slot }}
 </span>
--- resources/views/components/badge/outline.blade.php
+++ resources/views/components/badge/outline.blade.php
@@ -1,7 +1,9 @@
 @props([
   'color' => 'default',
   'scrollable' => false,
+  'sm' => false,
+  'lg' => false
 ])
-<span {{ $attributes->class([ 'badge badge-outline' => true, 'text-'.$color => $color ]) }}>
+<span {{ $attributes->class([ 'badge badge-outline' => true, 'text-'.$color => $color, 'badge-sm' => $sm, 'badge-lg' => $lg ]) }}>
   {{ $slot }}
 </span>
