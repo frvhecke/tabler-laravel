ad3d5989549f40b5831d97c181343668c76b4482
Frederik Van Hecke <frederik@kluster.dev>
2025-03-08 10:43:09 +0100

    Changing form label text to slot

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,5 +1,4 @@
 @props([
-	'text',                // Text for the label
 	'required' => false,   // Flag indicating whether to render label in required state
 ])
-<label @class(['form-label', 'required' => $required])>{{ $text }}</label>
+<label @class(['form-label', 'required' => $required])>{{ $slot }}</label>
