6c403d2687561fa2a78d2b8daf8fb0c178c930ce
Frederik Van Hecke <frederik@kluster.dev>
2025-03-14 11:00:10 +0100

    Properly propagating attributes

--- resources/views/components/form/toggle.blade.php
+++ resources/views/components/form/toggle.blade.php
@@ -6,7 +6,7 @@
   'disabled' => false,    // Flag indicating whether the toggle switch should be disabled
   'inline' => false       // Flag indicating whether the toggle switch should be displayed inline
 ])
-<label @class(['form-check form-switch', 'form-check-inline' => $inline])>
+<label {{ $attributes->class([ 'form-check form-switch', 'form-check-inline' => $inline ]) }}>
   <input class="form-check-input" type="checkbox" @if(!empty($name)) name="{{ $name }}" @endif {{ $selected?"checked":"" }} {{ $disabled?"disabled":"" }}>
   <span class="form-check-label">{{ $text }}</span>
   @if(!empty($description))
