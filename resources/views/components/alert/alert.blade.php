71a87a3d0c91070cfb86cecf55c0d584951abcb2
Frederik Van Hecke <frederik@kluster.dev>
2025-03-15 15:21:57 +0100

    Fix on alert

--- resources/views/components/alert/alert.blade.php
+++ resources/views/components/alert/alert.blade.php
@@ -7,21 +7,19 @@
   'important' => false    // Flag indicating whether alert is important (using color as background for alert box)
 ])
 <div role="alert" @class(['alert', 'alert-'.$color => !empty($color), 'alert-dismissible' => $dismissible, 'alert-important' => $important])>
-  <div class="d-flex">
-    @if(!empty($icon))
-    <x-icon.tabler :icon="$icon" class="icon alert-icon" />
+  @if(!empty($icon))
+  <x-icon.tabler :icon="$icon" class="icon alert-icon" />
+  @endif
+  @if(empty($description))
+  <div>{{ $title }}</div>
+  @else
+  <div>
+    @if(!empty($title))
+    <h4 class="alert-title">{{ $title }}</h4>
     @endif
-    @if(empty($description))
-    <div>{{ $title }}</div>
-    @else
-    <div>
-      @if(!empty($title))
-      <h4 class="alert-title">{{ $title }}</h4>
-      @endif
-      <div class="text-secondary">{{ $description }}</div>
-    </div>
-    @endif
+    <div class="text-secondary">{{ $description }}</div>
   </div>
+  @endif
 @if($dismissible)
   <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
 @endif
