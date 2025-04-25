74ec40b68549b3e2c739e040a1cb15b43086d6f0
Frederik Van Hecke <frederik@kluster.dev>
2025-04-25 17:48:46 +0200

    Adding disabled and readonly props

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -2,9 +2,11 @@
   'name',                 // Name for the input field
   'options' => [],        // List of options for the select field (value and text)
   'selected',             // Selected value
-  'rounded' => false      // Flag indicating whether style is rounded
+  'rounded' => false,     // Flag indicating whether style is rounded
+  'disabled' => false,    // Flag indicating whether field is disabled
+  'readonly' => false,    // Flag indicating whether field is read-only
 ])
-<select @class(["form-select", "form-control-rounded" => $rounded]) @if(!empty($name)) name="{{ $name }}" @endif>
+<select @class(["form-select", "form-control-rounded" => $rounded]) @if(!empty($name)) name="{{ $name }}" @endif @disabled($disabled) @readonly($readonly)>
 @foreach($options as $key => $option)
   <option value="{{ $key }}" {{ ($key == $selected)?"selected":"" }}>{{ $option }}</option>
 @endforeach
