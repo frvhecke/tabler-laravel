7b5ba61023461ad056354cd0a957b76cf200c2b3
Frederik Van Hecke <frederik@kluster.dev>
2025-04-25 17:38:12 +0200

    Adding option to disable hover background

--- resources/views/components/list/item.blade.php
+++ resources/views/components/list/item.blade.php
@@ -1,6 +1,7 @@
 @props([
   'flush' => false,
+  'hover' => true
 ])
-<div {{ $attributes->class([ 'list-group-item' => true, 'list-group-flush' => $flush ]) }}>
+<div {{ $attributes->class([ 'list-group-item' => true, 'list-group-flush' => $flush ]) }} @if(!$hover) style="background-color: unset !important;" @endif>
   {{ $slot }}
 </div>
