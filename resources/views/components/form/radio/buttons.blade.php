e68f33783a6ef63c8eb026ac3510fbd0842fbeb8
Frederik Van Hecke <frederik@kluster.dev>
2025-04-26 11:52:04 +0200

    Adding disabled option

--- resources/views/components/form/radio/buttons.blade.php
+++ resources/views/components/form/radio/buttons.blade.php
@@ -2,10 +2,11 @@
   'name',                 // Name for the input field
   'options' => [],        // List of options for the select field (value and text)
   'selected' => '',       // Selected value
+  'disabled' => false,    // Flag indicating whether field is disabled
 ])
 <div class="btn-group w-100" role="group">
 @foreach($options as $key => $option)
-  <input type="radio" class="btn-check" name="{{ $name }}" id="{{ $name }}-{{ $key }}" autocomplete="off" {{ ($key == $selected)?"checked":"" }}>
+  <input type="radio" class="btn-check" name="{{ $name }}" id="{{ $name }}-{{ $key }}" autocomplete="off" {{ ($key == $selected)?"checked":"" }} @disabled($disabled)>
   <label for="{{ $name }}-{{ $key }}" type="button" class="btn">{{ $option }}</label>
 @endforeach
 </div>
